f497887b73b3c7295be9615e6f9bfa9574f609f8
Dev Krishna Gupta <dev.trisect@gmail.com>
2023-11-14 15:57:40 +0530

    CombiningTwoListUsingStream

---
+++ functional-programming-in-java/CombiningTwoListUsingStream.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CombiningTwoListUsingStream {
+    public static void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(1,2,3,4,5);
+        List<Integer> list2 = Arrays.asList(6,7,8,9,10);
+        List<Integer> ans = Stream.concat(list2.stream(), list1.stream())
+                            // .sorted(Comparator.comparingInt(n->n))
+                            // .sorted((n1, n2)->n1-n2 > 0 ? 1 : -1)
+                            .sorted((n1, n2)->Integer.compare(n1, n2))
+                            .collect(Collectors.toList());
+        System.out.println(ans);
+    }
+}
--- functional-programming-in-java/ThirdHighestFrequency.java
+++ functional-programming-in-java/ThirdHighestFrequency.java
@@ -30,9 +30,9 @@ public class ThirdHighestFrequency {
     public static void main(String[] args) {
         int[] arr = {1,1,2,2,3,2,1,1,3,4,5};
         int result1 = findThirdHighestFrequencyInArray(arr);
-        System.out.println(result1);
+        System.out.println("In array " + result1);
         List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
         int result2 = findThirdHighestFrequencyInList(list);
-        System.out.println(result2);
+        System.out.println("In list " + result2);
     }
 }
