78b1977406857cd7e716d67b5bf1a533cd6807ca
Dev Krishna Gupta <dev.trisect@gmail.com>
2023-11-21 23:48:04 +0530

    PrintEvenOddUsingTwoThread

---
+++ threads/deadlockFree/PrintEvenOddUsingTwoThread.java
@@ -0,0 +1,52 @@
+package threads.deadlockFree;
+
+import javax.management.RuntimeErrorException;
+
+class EvenOddPrinting implements Runnable{
+
+    static int counter = 1;
+    Object lock;
+
+    public EvenOddPrinting(Object lock){
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Run method started by "+Thread.currentThread().getName());
+        while (counter < 10) {
+            if (Thread.currentThread().getName().equals("even") && counter%2==0) {
+                synchronized(lock){
+                    System.out.println("Printing by thread "+Thread.currentThread().getName()+" and Number is "+counter);
+                    counter++;
+                    lock.notify();
+                }
+            }
+            if (Thread.currentThread().getName().equals("odd") && counter%2!=0) {
+                synchronized(lock){
+                    System.out.println("Printing by thread "+Thread.currentThread().getName()+" and Number is "+counter);
+                    counter++;
+                    try{
+                        lock.wait();
+                    }catch(InterruptedException e){
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+public class PrintEvenOddUsingTwoThread {
+    public static void main(String[] args) {
+        Object lock = new Object();
+        Runnable run1 = new EvenOddPrinting(lock);
+        Runnable run2 = new EvenOddPrinting(lock);
+        Thread thread1 = new Thread(run1, "even");
+        Thread thread2 = new Thread(run2, "odd");
+        thread1.start();
+        thread2.start();
+    }
+
+}
